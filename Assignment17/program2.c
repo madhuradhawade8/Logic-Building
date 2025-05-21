ee29e6a010d1a495cab9cda607102f850ded74e8
madhura <madhura.dhawade24@gmail.com>
2025-05-21 23:22:30 +0530

    Assignement-17 : Problem-2

---
+++ Assignment17/program2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    for (int i = 1; i <= iRow; i++)
+    {
+        for (int j = iCol; j >= 1; j--)
+        {
+            if (i == j)
+            {
+                printf("#\t");
+            }
+            else if (i <= j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("@\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows & columns: ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
